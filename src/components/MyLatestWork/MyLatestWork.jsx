e0919ee17f3d63897051f41347a0c3d7fe5e380b
Muhammad Zohaib <zohaibsoomro100@gmail.com>
2026-03-11 17:46:47 +0500

    LATEST WORK

--- src/components/MyLatestWork/MyLatestWork.jsx
+++ src/components/MyLatestWork/MyLatestWork.jsx
@@ -1,8 +1,62 @@
 import './mylatestwork.css'
 
 const MyLatestWork = () => {
+
+
+  let latestWorkData = [
+    {
+      id: 1,
+      name: "E-commerce Website",
+      picture: "https://greatstack.in/portfolio/assets/project_1-5xYOfh0z.svg",
+    },
+    {
+      id: 2,
+      name: "Portfolio Website",
+      picture: "https://greatstack.in/portfolio/assets/project_2-ko3fLZz-.svg",
+    },
+    {
+      id: 3,
+      name: "Blog Platform",
+      picture: "https://greatstack.in/portfolio/assets/project_3-G9x9U39U.svg",
+    },
+    {
+      id: 4,
+      name: "Mobile App",
+      picture: "https://greatstack.in/portfolio/assets/project_4-KRxErSUy.svg",
+    },
+    {
+      id: 5,
+      name: "Dashboard Design",
+      picture: "https://greatstack.in/portfolio/assets/project_6-VGGcxYRF.svg",
+    },
+    {
+      id: 6,
+      name: "Portfolio Website",
+      picture: "https://greatstack.in/portfolio/assets/project_2-ko3fLZz-.svg",
+    },
+  ]
+
   return (
-    <div>MyLatestWork</div>
+    <div className='latestwork'>
+
+      <h1>My Latest Work</h1>
+
+
+      <div className='cards-container'>
+
+        {
+          latestWorkData.map((item) => (
+            <div className='latestwork-ui'>
+              <h2 className=''>0{item.id}</h2>
+              <h1 className='gradient-text'>{item.name}</h1>
+              <img src={item.picture} alt={item.name} />
+            </div>
+          ))
+        }
+
+
+      </div>
+    </div>
   )
 }
 
--- src/components/MyServices/MyServices.jsx
+++ src/components/MyServices/MyServices.jsx
@@ -1,8 +1,70 @@
 import './myservices.css'
 
 const MyServices = () => {
+
+  let servicesData = [
+    {
+      id: 1,
+      name: "Web Development",
+      description: "Building responsive and modern websites.",
+      price: "$1000"
+    },
+
+    {
+      id: 2,
+      name: "Graphic Design",
+      description: "Creating visually appealing designs for your brand.",
+      price: "$500"
+    },
+    {
+      id: 3,
+      name: "SEO Optimization",
+      description: "Improving your website's visibility on search engines.",
+      price: "$800"
+    },
+    {
+      id: 4,
+      name: "Content Writing",
+      description: "Crafting engaging and SEO-friendly content for your website.",
+      price: "$300"
+    },
+    {
+      id: 5,
+      name: "Social Media Management",
+      description: "Managing your social media accounts and creating content.",
+      price: "$600"
+    },
+    {
+      id: 6,
+      name: "App Development",
+      description: "Developing mobile applications for iOS and Android.",
+      price: "$2000"
+    }
+
+  ]
+
+
   return (
-    <div>MyServices</div>
+    <div className='services'>
+      <h1>My Services</h1>
+
+      <div className='cards-container'>
+
+        {
+          servicesData.map((item) => (
+            <div className='card-ui'>
+              <h2>0{item.id}</h2>
+              <h1 className='gradient-text'>{item.name}</h1>
+              <p>{item.description}</p>
+              <span className='gradient-text'>{item.price}</span>
+            </div>
+          ))
+        }
+
+
+      </div>
+
+    </div>
   )
 }
 
